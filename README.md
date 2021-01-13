1bef63653f7db660f23e68204b5dcbd8e31aa140
rocord01 <57117549+rocord01@users.noreply.github.com>
2021-01-13 11:37:42 -0500

    Edit CSS, JS, HTML

--- README.md
+++
@@ -1,4 +0,0 @@
-# QR-Code-Generator
-### url = https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
-
-<img src="snap.PNG">
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 function func(){
     var x = document.getElementById("input").value;
-    var url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${x}`;
+    var url = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${x}`;
 
     var img = document.getElementById("img");
-
+    var generatedText = document.getElementById('successText');
+    generatedText.classList.remove("hide")
     img.src = url;
+    generatedText.innerHTML = "Generated successfully!";
     console.log(url);
   }
